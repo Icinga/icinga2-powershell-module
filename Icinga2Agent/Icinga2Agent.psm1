3fd8c59d16f46ef6ad0cb853b6c9e595eb1ea470
Michael Friedrich <michael.friedrich@netways.de>
2020-02-13 13:10:34 +0100

    Fix missing icinga2.conf ITL includes
    This mimics https://github.com/Icinga/icinga2/blob/master/etc/icinga2/win32/icinga2.conf

--- Icinga2Agent/Icinga2Agent.psm1
+++ Icinga2Agent/Icinga2Agent.psm1
@@ -1589,8 +1589,10 @@ object ApiListener "api" {' + $certificateConfig + '
 include "constants.conf"
 include <itl>
 include <plugins>
-include <nscp>
+include <plugins-contrib>
+include <manubulon>
 include <windows-plugins>
+include <nscp>
 
 /* Required for Icinga 2.8.0 and above */
 const NodeName = "' + $this.getProperty('local_hostname') + '"
